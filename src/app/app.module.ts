31cf97d5d5404ae37051b35683806f43029c5635
Francis Nepomuceno <francisnepo@gmail.com>
2019-04-16 12:19:53 -0400

    Added event registration form Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,32 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventListComponent } from './event-list/event-list.component';
 import { EventDetailComponent } from './event-detail/event-detail.component';
+import { EventRegistrationFormComponent } from './event-registration-form/event-registration-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventListComponent,
-    EventDetailComponent
+    EventDetailComponent,
+    EventRegistrationFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
-    MatGridListModule
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/event-registration-form/event-registration-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-registration-form',
+  templateUrl: './event-registration-form.component.html',
+  styleUrls: ['./event-registration-form.component.css']
+})
+export class EventRegistrationFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
