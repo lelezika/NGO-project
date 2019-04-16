ea09af4c1d06e7a228121e0a99a96d61b44e6d31
Francis Nepomuceno <francisnepo@gmail.com>
2019-04-16 19:19:14 -0400

    Converted registration form into a Reactive Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatButtonModule } from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventListComponent } from './event-list/event-list.component';
 import { EventDetailComponent } from './event-detail/event-detail.component';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatTableModule} from '@angular/material/table';
-import { MatDialogModule } from '@angular/material/dialog';
-
 import { EventManagementComponent } from './event-management/event-management.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { EventRegistrationFormComponent } from './event-registration-form/event-registration-form.component';
 import { UserManagementComponent } from './user-management/user-management.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -46,7 +48,9 @@ import { HttpClientModule } from '@angular/common/http';
     MatFormFieldModule,
     MatInputModule,
     MatToolbarModule,
-    MatDialogModule
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   entryComponents: [
     EventRegistrationFormComponent
