f13c054a9f5f46758ade4d5fe39f0030a38d8388
Francis Nepomuceno <francisnepo@gmail.com>
2019-04-17 14:27:23 -0400

    Fixed unmerged changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
-import { UserManagementComponent } from './user-management/user-management.component';
-import { EventListComponent } from './event-list/event-list.component';
-import { EventDetailComponent } from './event-detail/event-detail.component';
-import { EventManagementComponent } from './event-management/event-management.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AdminComponent } from './admin/admin.component';
-import { EventCreateComponent } from './event-create/event-create.component';
-import { UserViewComponent } from './user-view/user-view.component';
-
-
-const routes: Routes = [
-  { path: '', redirectTo: '/eventlist', pathMatch: 'full' },
-  {path: 'admin', component: AdminComponent, children: [
-=======
 import { AdminComponent } from './admin/admin.component';
 import { EventCreateComponent } from './event-create/event-create.component';
 import { EventDetailComponent } from './event-detail/event-detail.component';
@@ -27,7 +12,6 @@ import { UserViewComponent } from './user-view/user-view.component';
 const routes: Routes = [
   { path: '', redirectTo: '/eventlist', pathMatch: 'full' },
   {path: 'admin', component: AdminComponent, canActivate: [], children: [
->>>>>>> 065079954eedf8bc9cf57a1a8ce63b7876eec243
     {path: 'user-view', component: UserViewComponent},
     {path: 'user-management', component: UserManagementComponent},
     {path: 'event-management', component: EventManagementComponent},
@@ -35,14 +19,9 @@ const routes: Routes = [
     {path: 'edit-event/:eventId', component: EventCreateComponent}
   ]},
   { path: 'eventlist', component: EventListComponent },
-<<<<<<< HEAD
-  { path: 'eventdetail', component: EventDetailComponent },
-  { path: '**', component:PageNotFoundComponent }
-=======
   { path: 'events/:id', component: EventDetailComponent },
   { path: 'page-not-found', component: PageNotFoundComponent },
   { path: '**', redirectTo: '/page-not-found', pathMatch: 'full' }
->>>>>>> 065079954eedf8bc9cf57a1a8ce63b7876eec243
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-<<<<<<< HEAD
-import { MatButtonModule } from '@angular/material';
-import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatToolbarModule } from '@angular/material/toolbar';
-=======
 import {
   MatButtonModule,
   MatCardModule,
@@ -29,30 +21,18 @@ import {
   MatSidenavModule,
   MatStepperModule,
 } from '@angular/material';
->>>>>>> 065079954eedf8bc9cf57a1a8ce63b7876eec243
 
 import { AdminComponent } from './admin/admin.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventCreateComponent } from './event-create/event-create.component';
 import { EventDetailComponent } from './event-detail/event-detail.component';
-<<<<<<< HEAD
-import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatDialogModule} from '@angular/material/dialog';
-import { MatTableModule} from '@angular/material/table';
-=======
 import { EventListComponent } from './event-list/event-list.component';
->>>>>>> 065079954eedf8bc9cf57a1a8ce63b7876eec243
 import { EventManagementComponent } from './event-management/event-management.component';
 import { EventRegistrationFormComponent } from './event-registration-form/event-registration-form.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { UserManagementComponent } from './user-management/user-management.component';
-<<<<<<< HEAD
-import { EventCreateComponent } from './event-create/event-create.component';
-import { AdminComponent } from './admin/admin.component';
-=======
 import { UserDetailsComponent } from './user-details/user-details.component';
->>>>>>> 065079954eedf8bc9cf57a1a8ce63b7876eec243
 import { UserViewComponent } from './user-view/user-view.component';
 
 @NgModule({
@@ -65,16 +45,9 @@ import { UserViewComponent } from './user-view/user-view.component';
     EventManagementComponent,
     EventRegistrationFormComponent,
     UserManagementComponent,
-<<<<<<< HEAD
-    EventCreateComponent,
-    AdminComponent,
-    UserViewComponent,
-    PageNotFoundComponent
-=======
     UserDetailsComponent,
     PageNotFoundComponent,
     UserViewComponent
->>>>>>> 065079954eedf8bc9cf57a1a8ce63b7876eec243
   ],
   imports: [
     AppRoutingModule,
@@ -94,14 +67,9 @@ import { UserViewComponent } from './user-view/user-view.component';
     MatProgressSpinnerModule,
     MatTableModule,
     MatToolbarModule,
-<<<<<<< HEAD
-    MatDialogModule,
-    FormsModule,
-=======
     MatSelectModule,
     MatSidenavModule,
     MatStepperModule,
->>>>>>> 065079954eedf8bc9cf57a1a8ce63b7876eec243
     ReactiveFormsModule
   ],
   entryComponents: [
