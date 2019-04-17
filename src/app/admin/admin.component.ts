0dcee615e6ce160eaa8c154e8283a51d2165b3a5
will <mortalswill@gmail.com>
2019-04-17 14:04:02 -0400

    major change5

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { EventService } from '../event.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  selectedModule: string;
+  headerFlag = false;
+
+  mobileQuery: MediaQueryList;
+  fillerNav: string[] = ['User Management', 'Event Management', 'User View'];
+
+  private _mobileQueryListener: () => void;
+
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    private eventsService: EventService
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
   ngOnInit() {
+    this.headerFlag = false;
+    this.eventsService.moduleHeader.subscribe(header => this.selectedModule = header);
+  }
+
+  onModuleSelected(selectedModule) {
+    this.eventsService.setModuleHeader(selectedModule);
+    this.headerFlag = true;
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserManagementComponent } from './user-management/user-management.component';
 import { EventListComponent } from './event-list/event-list.component';
 import { EventDetailComponent } from './event-detail/event-detail.component';
-<<<<<<< HEAD
-import { EventManagementComponent } from "./event-management/event-management.component";
-import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-import { EventCreateComponent } from './event-create/event-create.component';
-=======
 import { EventManagementComponent } from './event-management/event-management.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
->>>>>>> ea09af4c1d06e7a228121e0a99a96d61b44e6d31
+import { AdminComponent } from './admin/admin.component';
+import { EventCreateComponent } from './event-create/event-create.component';
+import { UserViewComponent } from './user-view/user-view.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/eventlist', pathMatch: 'full' },
-<<<<<<< HEAD
-  {path: 'admin', component: Admin, canActivate: [AdminAuthGuard], children: [
+  {path: 'admin', component: AdminComponent, children: [
     {path: 'user-view', component: UserViewComponent},
     {path: 'user-management', component: UserManagementComponent},
     {path: 'event-management', component: EventManagementComponent},
@@ -26,14 +22,6 @@ const routes: Routes = [
   { path: 'eventlist', component: EventListComponent },
   { path: 'eventdetail', component: EventDetailComponent },
   { path: '**', component:PageNotFoundComponent }
-=======
-  { path: 'api/users', component: UserManagementComponent },
-  { path: 'api/event', component: EventManagementComponent },
-  { path: 'eventlist', component: EventListComponent },
-  { path: 'events/:id', component: EventDetailComponent },
-  { path: 'page-not-found', component: PageNotFoundComponent },
-  { path: '**', redirectTo: '/page-not-found', pathMatch: 'full' }
->>>>>>> ea09af4c1d06e7a228121e0a99a96d61b44e6d31
 ];
 
 @NgModule({
