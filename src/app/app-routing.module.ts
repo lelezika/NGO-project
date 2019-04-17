ec9812702bc95a373d8e8156cb1f1e8715428e5a
Francis Nepomuceno <francisnepo@gmail.com>
2019-04-17 13:17:43 -0400

    Fixed front-end issues and cleaned up imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserManagementComponent } from './user-management/user-management.component';
-import { EventListComponent } from './event-list/event-list.component';
-import { EventDetailComponent } from './event-detail/event-detail.component';
-<<<<<<< HEAD
-import { EventManagementComponent } from "./event-management/event-management.component";
-import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { AdminComponent } from './admin/admin.component';
 import { EventCreateComponent } from './event-create/event-create.component';
-=======
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { EventListComponent } from './event-list/event-list.component';
 import { EventManagementComponent } from './event-management/event-management.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
->>>>>>> ea09af4c1d06e7a228121e0a99a96d61b44e6d31
-
+import { UserManagementComponent } from './user-management/user-management.component';
+import { UserViewComponent } from './user-view/user-view.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/eventlist', pathMatch: 'full' },
-<<<<<<< HEAD
-  {path: 'admin', component: Admin, canActivate: [AdminAuthGuard], children: [
+  {path: 'admin', component: AdminComponent, canActivate: [], children: [
     {path: 'user-view', component: UserViewComponent},
     {path: 'user-management', component: UserManagementComponent},
     {path: 'event-management', component: EventManagementComponent},
@@ -24,16 +19,9 @@ const routes: Routes = [
     {path: 'edit-event/:eventId', component: EventCreateComponent}
   ]},
   { path: 'eventlist', component: EventListComponent },
-  { path: 'eventdetail', component: EventDetailComponent },
-  { path: '**', component:PageNotFoundComponent }
-=======
-  { path: 'api/users', component: UserManagementComponent },
-  { path: 'api/event', component: EventManagementComponent },
-  { path: 'eventlist', component: EventListComponent },
   { path: 'events/:id', component: EventDetailComponent },
   { path: 'page-not-found', component: PageNotFoundComponent },
   { path: '**', redirectTo: '/page-not-found', pathMatch: 'full' }
->>>>>>> ea09af4c1d06e7a228121e0a99a96d61b44e6d31
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,79 +4,74 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatButtonModule } from '@angular/material';
-import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatTableModule } from '@angular/material/table';
-import { MatDialogModule } from '@angular/material/dialog';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatProgressSpinnerModule,
+  MatTableModule,
+  MatToolbarModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatStepperModule,
+} from '@angular/material';
 
+import { AdminComponent } from './admin/admin.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventListComponent } from './event-list/event-list.component';
-import { EventDetailComponent } from './event-detail/event-detail.component';
-<<<<<<< HEAD
-import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatDialogModule} from '@angular/material/dialog';
-import { MatTableModule} from '@angular/material/table';
-=======
->>>>>>> ea09af4c1d06e7a228121e0a99a96d61b44e6d31
-import { EventManagementComponent } from './event-management/event-management.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { EventRegistrationFormComponent } from './event-registration-form/event-registration-form.component';
-import { UserManagementComponent } from './user-management/user-management.component';
-<<<<<<< HEAD
-import { HttpClientModule } from '@angular/common/http';
 import { EventCreateComponent } from './event-create/event-create.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { EventManagementComponent } from './event-management/event-management.component';
+import { EventRegistrationFormComponent } from './event-registration-form/event-registration-form.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UserManagementComponent } from './user-management/user-management.component';
 import { UserViewComponent } from './user-view/user-view.component';
-=======
->>>>>>> ea09af4c1d06e7a228121e0a99a96d61b44e6d31
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventListComponent,
+    AdminComponent,
+    EventCreateComponent,
     EventDetailComponent,
+    EventListComponent,
     EventManagementComponent,
     EventRegistrationFormComponent,
-<<<<<<< HEAD
-    UserManagementComponent,
-    EventCreateComponent,
-    UserViewComponent
-=======
     PageNotFoundComponent,
-    UserManagementComponent
->>>>>>> ea09af4c1d06e7a228121e0a99a96d61b44e6d31
+    UserManagementComponent,
+    UserViewComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
     AppRoutingModule,
+    BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
     MatButtonModule,
     MatCardModule,
-    MatGridListModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatTableModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatToolbarModule,
-<<<<<<< HEAD
-    MatButtonModule,
-    MatDialogModule
-=======
+    MatDatepickerModule,
     MatDialogModule,
-    FormsModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatStepperModule,
     ReactiveFormsModule
   ],
   entryComponents: [
     EventRegistrationFormComponent
->>>>>>> ea09af4c1d06e7a228121e0a99a96d61b44e6d31
   ],
   providers: [],
   bootstrap: [AppComponent]
