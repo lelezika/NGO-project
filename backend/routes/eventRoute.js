205430f29b7cf8edfb72e1bfc85fa132a28ef22c
will <mortalswill@gmail.com>
2019-04-19 14:59:18 -0400

    le 68

--- backend/routes/eventRoute.js
+++ backend/routes/eventRoute.js
@@ -1,41 +1,16 @@
 const express = require("express");
-const multer = require("multer");
 
 const Event = require('../models/eventModel');
 const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg"
-};
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime type");
-    if (isValid) {
-      error = null;
-    }
-    cb(error, "backend/images");
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname
-      .toLowerCase()
-      .split(" ")
-      .join("-");
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + "." + ext);
-  }
-});
 
 // Post new Event
 router.post(
   "",
   checkAuth,
-  multer({ storage: storage }).single("image"),
+
   (req, res, next) => {
     const url = req.protocol + "://" + req.get("host");
     const event = new Event({
@@ -50,7 +25,7 @@ router.post(
       endTime: req.body.endTime,
       adultTicketPrice: req.body.adultTicketPrice,
       childTicketPrice: req.body.childTicketPrice,
-      imagePath: url + "/images/" + req.file.filename
+      imagePath: req.body.imagePath
     });
     event.save().then(createdEvent => {
       res.status(201).json({
@@ -68,12 +43,10 @@ router.post(
 router.put(
   '/:id',
   checkAuth,
-  multer({storage: storage}).single("image"),
   (req,res,next) => {
     let imagePath = req.body.imagePath;
     if(req.file){
       const url = req.protocol + "://" + req.get("host");
-      imagePath = url + "/images/" + req.file.filename
     }
     const event = new Event({
       _id: req.body.id,
