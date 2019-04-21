8894788bdaaf07ccd0e15243c8dd62a0bac28b7a
will <mortalswill@gmail.com>
2019-04-21 17:55:09 -0400

    done with user delete

--- src/app/user-delete/user-delete.component.ts
+++ src/app/user-delete/user-delete.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-delete',
@@ -6,10 +11,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-delete.component.css']
 })
 export class UserDeleteComponent implements OnInit {
+  constructor(
+    private http: HttpClient,
+    private userServices: UserService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
-  constructor() { }
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  role: string;
+  retrived_user: User = new User();
 
   ngOnInit() {
-  }
+    // //Fetch the Email from the activated route
+    this.activatedRoute.params.subscribe(
+      params => (this.email = params["email"])
+    );
 
+    this.userServices.getUserByEmail(this.email).subscribe((data:User) => {
+      this.retrived_user = data;
+      this.firstName = data.firstName;
+      this.lastName = data.lastName;
+      this.password = data.password;
+      this.role = data.role;
+    });
+  }
+  
+
+  onSubmit(userForm) {
+
+    this.userServices.deleteUser(this.email).subscribe(
+      res => {
+        // alert("Successfully deleted the user");
+        this.router.navigate(["admin/user-management"]);
+      },
+      error => {
+        console.log("Error while deleting this user");
+      }
+    );
+  }
 }
+
+
